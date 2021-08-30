26da084d2a448b08a4707699d0c65826d94ff11e
hyokyun <gyrbs22@gmail.com>
2021-08-30 20:38:10 +0900

    after using redux : second, how to get updated data by using action(call reducer)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 
-const reducer = (state = 0) => {
+const reducer = (state = 0, action) => {
+  if(action.type === "ADD"){
+    state = state + 1
+  }
+  if(action.type === "MINUS"){
+    state = state - 1
+  }
+  console.log(state)
   return state
 }
 const countStore = createStore(reducer)
+
+countStore.dispatch({type : "ADD"})

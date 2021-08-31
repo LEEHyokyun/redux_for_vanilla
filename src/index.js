8e585b821d8050774d99d775681495b3a1315009
hyokyun <gyrbs22@gmail.com>
2021-08-31 16:06:36 +0900

    input value logic(type) / delete value logic(type, id)

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const DELETE_TODO = "DELETE_TODO"
 const reducer = (state = [], action) => {
   console.log(action)
   switch(action.type){
-    case ADD_TODO : return []
-    case DELETE_TODO : return []
+    case ADD_TODO : return [{ text : action.text, id : action.id }, ...state]
+    case DELETE_TODO : return state.filter(deletedList => deletedList.id !== Number(action.id))
     default : return state
   }
 }
@@ -22,11 +22,37 @@ const onSubmit = e => {
   const toDO = input.value
   input.value = ""
 
-  store.dispatch({type : "ADD_ToDo", text : toDO})
+  store.dispatch({type : "ADD_TODO", text : toDO, id: Date.now()})
 }
 
 form.addEventListener("submit", onSubmit)
 
-
 const store = createStore(reducer)
+
+const paintTextToView = () => {
+  const lists = store.getState()
+  ul.innerHTML = ""
+
+  lists.forEach(listItem => {
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+
+    li.id = listItem.id
+    li.innerText = listItem.text
+    ul.appendChild(li)
+    
+    btn.innerText = "DELETE the list"
+    btn.addEventListener("click", (e) => {
+      const id = e.target.parentNode.id
+      store.dispatch({ type : "DELETE_TODO", id })
+    })
+    li.appendChild(btn)
+  })
+}
+
+store.subscribe(paintTextToView)
+
+
+
+
 
